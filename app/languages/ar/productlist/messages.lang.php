1ecd740a9da37f39d9e755380da348f5df3ce881
ahmedsaadsalim <ahmedsaadsaleem@gmail.com>
2022-04-22 21:43:18 +0200

    added new languges files

---
+++ app/languages/ar/productlist/messages.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$_['message_create_success'] = 'تم حفظ المنتج بنجاح';
+$_['message_create_failed'] = 'خطأ في حفظ المنتج';
+$_['message_edit_success'] = 'تم تعديل بيانات المنتج بنجاح';
+$_['message_edit_failed'] = 'خطأ في تعديل بيانات المنتج';
+$_['message_delete_success'] = 'تم حذف المنتج بنجاح';
+$_['message_delete_failed'] = 'حدث خطأ أثناء حذف المنتج';
+$_['message_user_exists'] = 'المنتج موجود بالفعل';
---
+++ app/languages/en/productlist/default.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$_['title'] = 'المنتجات';
+$_['text_header'] = 'المنتجات';
+$_['text_add_item'] = 'إضافة منتج جديد';
+$_['text_table_name'] = 'اسم المنتج';
+$_['text_table_category'] = 'القسم التابع له';
+$_['text_table_buy_price'] = 'سعر الشراء';
+$_['text_table_sell_price'] = 'سعر البيع';
+$_['text_table_quantity'] = 'الكمية';
+$_['text_table_product_image'] = 'صورة القسم';
+$_['text_table_control'] = 'التحكم';
+$_['text_table_control_edit'] = 'تعديل';
+$_['text_table_control_delete'] = 'حذف';
+$_['text_control_delete_confirm'] = 'هل تود حذف هذا القسم؟';
+$_['text_data_not_exists'] = 'لا يوجد بيانات لعرضا';
