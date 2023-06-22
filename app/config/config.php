7e529e382e1b2f343942ff572cb26ce7060a3a85
Ahmed Saad Saleem <82448907+ahmedsaadsaleem@users.noreply.github.com>
2023-06-22 12:49:00 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -15,7 +15,7 @@ define('JS', DS . 'js' . DS);
 // Database Credentails
 defined('DATABASE_HOST_NAME')       ? null : define('DATABASE_HOST_NAME', 'localhost');
 defined('DATABASE_USER_NAME')       ? null : define('DATABASE_USER_NAME', 'root');
-defined('DATABASE_PASSWORD')        ? null : define('DATABASE_PASSWORD', 'hpl]control');
+defined('DATABASE_PASSWORD')        ? null : define('DATABASE_PASSWORD', '123456789');
 defined('DATABASE_DB_NAME')         ? null : define('DATABASE_DB_NAME', 'storedb');
 defined('DATABASE_PORT_NUMBER')     ? null : define('DATABASE_PORT_NUMBERE', 3307);
 defined('DATABASE_CONN_DRIVER')     ? null : define('DATABASE_CONN_DRIVER', 1);
